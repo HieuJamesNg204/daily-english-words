79b36a430181ed092b8b48e2b2a1554b5968d067
unknown <HieuJames204@gmail.com>
2025-12-17 16:12:21 +0700

    first commit

---
+++ src/main/java/com/hieujavahound/daily_english_words/DailyEnglishWordsApplication.java
@@ -0,0 +1,13 @@
+package com.hieujavahound.daily_english_words;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyEnglishWordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyEnglishWordsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hieujavahound/daily_english_words/DailyEnglishWordsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hieujavahound.daily_english_words;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyEnglishWordsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
